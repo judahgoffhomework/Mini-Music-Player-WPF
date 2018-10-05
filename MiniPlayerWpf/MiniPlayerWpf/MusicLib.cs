8e80b56384fdffadc2fde6e4a66b36c7b5db1b58
Judah Goff <bgoff1@harding.edu>
2018-10-05 16:30:33 -0500

    Initialize project with provided files.

---
+++ MiniPlayerWpf/MiniPlayerWpf/MusicLib.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPlayerWpf
+{
+    public class MusicLib
+    {
+        private DataSet musicDataSet;
+
+        public MusicLib()
+        {
+            musicDataSet = new DataSet();
+            musicDataSet.ReadXmlSchema("music.xsd");
+            musicDataSet.ReadXml("music.xml");
+        }
+
+        public void AddSong(Song song)
+        {
+            // Add the selected file to the song table
+            DataTable table = musicDataSet.Tables["song"];
+            DataRow row = table.NewRow();
+
+            row["title"] = song.Title;
+            row["artist"] = song.Artist;
+            row["album"] = song.Album;
+            row["filename"] = song.Filename;
+            row["length"] = song.Length;
+            row["genre"] = song.Genre;
+            table.Rows.Add(row);
+
+            // Now we know the ID
+            song.Id = Convert.ToInt32(row["id"]);
+        }
+
+        public Song GetSong(int songId)
+        {
+            Song song = null;
+            DataTable table = musicDataSet.Tables["song"];
+
+            // Only one row should be selected
+            foreach (DataRow row in table.Select("id=" + songId))
+            {
+                song = new Song();
+                song.Id = songId;
+                song.Title = row["title"].ToString();
+                song.Artist = row["artist"].ToString();
+                song.Album = row["album"].ToString();
+                song.Genre = row["genre"].ToString();
+                song.Length = row["length"].ToString();
+                song.Filename = row["filename"].ToString();
+            }
+
+            return song;
+        }
+    }
+}
